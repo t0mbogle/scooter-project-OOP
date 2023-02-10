a2cefff6b3e56328bed0d16e1fd489cf6dc039f8
Thomas Bogle <thomasbogle@Thomass-MacBook-Pro.local>
2023-02-10 16:15:57 +0000

    Added some tests for User class

--- tests/User.test.js
+++ tests/User.test.js
@@ -1,12 +1,34 @@
 const User = require('../src/User')
 
+
+describe('Test suite for User class', () => {
+    let user1; // Outside beforeEach so it can be referenced in the tests
+
+    beforeEach(() => {
+        user1 = {
+            username: "userName123",
+            password: "password123",
+            age: 30,
+            loggedIn: false,
+        };
+      });
+
+    test('Test to check the User has a username', () => {
+        expect(user1.username).toBe("userName123");
+    })
+    test('Test to check the User has a password', () => {
+        expect(user1.password).toBe("password123");
+    })
+    test('Test to check the User has an age', () => {
+        expect(user1.age).toBe(30);
+    })
+    test('Checks whether loggedIn is initially set to false', () => {
+        expect(user1.loggedIn).toBe(false);
+        expect(user1.loggedIn).not.toBe(true);
+    })
+})
+
 // User tests here
-
-// test username
-
-// test password
-
-// test age
 
 // test login
 
