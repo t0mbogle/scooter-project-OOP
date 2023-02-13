8092d7208a511fe751d9d932a63f3b81b33a3aac
Thomas Bogle <thomasbogle@Thomass-MacBook-Pro.local>
2023-02-13 11:07:29 +0000

    Added initial scooter object tests - TDD

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -3,11 +3,27 @@ const User = require('../src/User')
 
 //typeof scooter === object
 describe('scooter object', () => {
-  test('does something', () => {
-    // edit this to be a real test!
-    expect(false).toEqual(true);
-  }
-)
+  let scooter1;
+
+  beforeEach(() => {
+    scooter1 = {
+      station: ['Deansgate', 'Salford', 'Didsbury'],
+      user: null,
+    };
+  })
+
+  test('Tests wether scooter is an object', () => {
+    expect(scooter1).toBeInstanceOf(Object);
+  })
+  test('Checks if user is initially set to null', () => {
+    expect(scooter1.user).toBe(null);
+  })
+  test('Checks if scooter has initial stations', () => {
+    expect(scooter1.station).not.toBe(null);
+  })
+  test('Checks if scooter stations array has expected length', () => {
+    expect(scooter1.station).toHaveLength(3);
+  })
 })
 
 //Method tests
